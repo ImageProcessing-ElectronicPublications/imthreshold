3a92f5c8047025be920d3765b9f71aeb20645606
zvezdochiot <mykaralw@yandex.ru>
2017-04-12 22:44:22 +0400

    0.20170411

---
+++ ttsai.cpp
@@ -0,0 +1,150 @@
+//	This program is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program; if not, write to the Free Software
+//	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//	http://www.gnu.org/copyleft/gpl.html
+
+/*
+    Finds a threshold point using the Tsai Moment Preserving threshold
+    algorithm. Reference:
+
+    W.H. Tsai: *Moment-Preserving Thresholding: A New Approach.*
+    Computer Vision Graphics and Image Processing (29), pp. 377-393
+    (1985)
+*/
+
+// This algorithm was taken from the gamera.sf.net sourcecodes 
+// and adopted for the FreeImage library
+//
+// Copyright (C) 2007-2008:
+// monday2000  monday2000@yandex.ru
+
+#include <unistd.h>
+#include <FreeImage.h>
+#include "imthresholdfreeimage.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+void ImthresholdFilterTTsaiTitle()
+{
+	printf("ImThreshold.\n");
+	printf("BookScanLib Project: http://djvu-soft.narod.ru/\n\n");
+	printf("Finds a threshold point using the Tsai Moment Preserving threshold algorithm.\n");
+	printf("This algorithm was taken from the gamera.sf.net sourcecodes and adopted for the FreeImage library.\n\n");
+}
+
+void ImthresholdFilterTTsaiUsage()
+{
+	printf("Usage : imthreshold-ttsai [options] <input_file> <output_file>(BW)\n\n");
+	printf("options:\n");
+	printf("          -s N    shift (int, optional, default = -32)\n");
+	printf("          -h      this help\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[])
+{
+	// call this ONLY when linking with FreeImage as a static library
+#ifdef FREEIMAGE_LIB
+	FreeImage_Initialise();
+#endif // FREEIMAGE_LIB
+	
+	int opt;
+	int shift = -32;
+	bool fhelp = false;
+	int threshold = 0;
+	while ((opt = getopt(argc, argv, ":s:h")) != -1)
+	{
+		switch(opt)
+		{
+			case 's':
+				shift = atof(optarg);
+				break;
+			case 'h':
+				fhelp = true;
+				break;
+			case ':':
+				printf("option needs a value\n");
+				break;
+			case '?':
+				printf("unknown option: %c\n", optopt);
+				break;
+		}
+	}
+	
+	ImthresholdFilterTTsaiTitle();
+	
+	if(optind + 2 > argc || fhelp)
+	{
+		ImthresholdFilterTTsaiUsage();
+		return 0;
+	}
+	const char *src_filename = argv[optind];
+	const char *output_filename = argv[optind + 1];
+	
+	FreeImage_SetOutputMessage(FreeImageErrorHandler);
+	
+	printf("Input= %s\n", src_filename);
+	FIBITMAP *dib = ImthresholdGenericLoader(src_filename, 0);
+	if (dib)
+	{		
+		if (FreeImage_GetImageType(dib) == FIT_BITMAP)
+		{
+			FIBITMAP* dst_dib;
+			unsigned width = FreeImage_GetWidth(dib);
+			unsigned height = FreeImage_GetHeight(dib);
+			unsigned y;
+
+			IMTpixel** p_im;
+			p_im = (IMTpixel**)malloc(height * sizeof(IMTpixel*));
+			for (y = 0; y < height; y++) {p_im[y] = (IMTpixel*)malloc(width * sizeof(IMTpixel));}
+			BYTE** d_im;
+			d_im = (BYTE**)malloc(height * sizeof(BYTE*));
+			for (y = 0; y < height; y++) {d_im[y] = (BYTE*)malloc(width * sizeof(BYTE));}
+
+			printf("Shift= %d\n", shift);
+
+			ImthresholdGetData(dib, p_im);
+			FreeImage_Unload(dib);
+			threshold = IMTFilterTTsai(p_im, d_im, height, width, shift);
+			printf("Threshold= %d\n", threshold / 3);
+			for (y = 0; y < height; y++){free(p_im[y]);}
+			free(p_im);
+			dst_dib = FreeImage_Allocate(width, height, 1);
+			ImthresholdSetDataBW(dst_dib, d_im);
+			for (y = 0; y < height; y++){free(d_im[y]);}
+			free(d_im);
+			
+			if (dst_dib)
+			{					
+				FREE_IMAGE_FORMAT out_fif = FreeImage_GetFIFFromFilename(output_filename);
+				if(out_fif != FIF_UNKNOWN)
+				{
+					FreeImage_Save(out_fif, dst_dib, output_filename, 0);
+					printf("Output= %s\n\n", output_filename);
+				}
+				FreeImage_Unload(dst_dib);					
+			}
+		} else {
+			printf("%s\n", "Unsupported format type.");
+			FreeImage_Unload(dib);
+		}
+	}	 
+	
+	// call this ONLY when linking with FreeImage as a static library
+#ifdef FREEIMAGE_LIB
+	FreeImage_DeInitialise();
+#endif // FREEIMAGE_LIB
+	
+	return 0;
+}
